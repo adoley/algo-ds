5f20a755986a9eb24a7e99b20a651ab5a738c247
adoley <apollodoley3@gmail.com>
2025-10-08 23:59:58 +0530

    updated twoSum Approach2

--- src/main/java/twosum/Approach2.java
+++ src/main/java/twosum/Approach2.java
@@ -1,6 +1,8 @@
 package twosum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Algo
@@ -9,15 +11,15 @@ import java.util.Arrays;
  *      (b) Input array length should be greater than or equal to 2
  *      (c) Target should not be null
  *      If any of the validation fail then return [-1,-1]
- * (2) Sort the input array
+ * (2) Use Map to store the number as key and index as value
  * (3) For each element
- *      (a) Subtract this element from the target to get the other number
- *      (b) Find the other number from the elements in the array from the next index of the current element
- *      (c) If the other number is found then return the indices
+ *      (a) Subtract this element from the target to get the remaining number
+ *      (b) Check if the remaining number exist in map
+ *      (c) If the remaining number is found then return the indices
  * (4) If pair is not found return [-1,-1]
  *
- * T(n) = n * log(n) (log(n) is for binary search)
- * S(n) = 1
+ * T(n) = n
+ * S(n) = n because of Map
  */
 public class Approach2 {
     public static int[] twoSum(int[] nums, int target){
@@ -25,33 +27,18 @@ public class Approach2 {
         result[0]=-1;
         result[1]=-1;
         if (nums!=null && nums.length>=2){
-            Arrays.sort(nums);
-            for(int i=0; i< nums.length-1;i++){
+            Map<Integer,Integer> numsIndex=new HashMap<>();
+            for(int i=0; i< nums.length;i++){
                 int currentNumber=nums[i];
                 int otherNumber=target-currentNumber;
-                int otherNumberIndex=binarySearch(nums,i+1,nums.length-1,otherNumber);
-                if(otherNumberIndex!=-1){
-                    result[0]=i;
-                    result[1]=otherNumberIndex;
-                    break;
+                if(numsIndex.containsKey(otherNumber)){
+                      result[0]=i;
+                      result[1]=numsIndex.get(otherNumber);
+                      Arrays.sort(result);
+                      break;
+                }else {
+                    numsIndex.put(currentNumber,i);
                 }
-            }
-        }
-
-        return result;
-    }
-
-    private static int binarySearch(int[] nums, int beginIndex, int endIndex,int searchNumber){
-        int result=-1;
-        while (beginIndex<=endIndex){
-            int mid=(beginIndex+endIndex)/2;
-            if (nums[mid]==searchNumber){
-                result= mid;
-                break;
-            }else if (nums[mid]<searchNumber){
-                beginIndex=mid+1;
-            }else {
-                endIndex=mid-1;
             }
         }
 
--- src/main/java/twosum/Driver.java
+++ src/main/java/twosum/Driver.java
@@ -2,12 +2,12 @@ package twosum;
 
 public class Driver {
     public static void main(String[] args) {
-       int[] nums = {2,7,11,15};
-       int target = 9;
+//       int[] nums = {2,7,11,15};
+//       int target = 9;
 //       int[] nums={3,2,4};
 //       int target=6;
-//        int[] nums={3,3};
-//        int target=6;
+        int[] nums={3,3};
+        int target=6;
 //        int[] nums={4,4};
 //        int target=6;
 

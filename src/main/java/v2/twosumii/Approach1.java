14c80e9f692767a957b01fbc052bccfb168c2642
adoley <apollodoley3@gmail.com>
2026-03-03 22:03:33 +0530

    Added approach1 for two sum II

---
+++ src/main/java/v2/twosumii/Approach1.java
@@ -0,0 +1,54 @@
+package v2.twosumii;
+
+import java.util.Arrays;
+
+/**
+ * The approach is to use two pointers. leftPointer start at index 0 and rightPointer at last index
+ * compute the sum of element at index leftPointer and rightPointer
+ *      if the sum is greater than the target then decrement the rightPointer index by 1
+ *      if the sum is less than the target then increment the leftPointer index by 1
+ *      if the sum is equal to the target then return the leftPointer and rightPointer index as a result
+ *
+ * Run time complexity : O(n)
+ * Space complexity : O(1)
+ */
+public class Approach1 {
+    public static void main(String[] args) {
+//       int[] numbers = {2,7,11,15};
+//       int target = 9;  //[1,2]
+
+//        int[] numbers = {2,3,4};
+//        int target = 6;  //[1,3]
+
+        int[] numbers = {-1,0};
+        int target=-1;// [1,2]
+
+        System.out.println(Arrays.toString(twoSum(numbers,target)));
+    }
+
+    private static int[] twoSum(int[] numbers, int target) {
+
+        if(numbers!=null){
+            int leftPointer=0;
+            int rightPointer= numbers.length-1;
+
+            while (leftPointer<rightPointer){
+                int sum=numbers[leftPointer]+numbers[rightPointer];
+                if(sum==target){
+                    int[] result=new int[2];
+                    result[0]=leftPointer+1;
+                    result[1]=rightPointer+1;
+
+                    return result;
+                }
+
+                if(sum>target){
+                    rightPointer--;
+                }else {
+                    leftPointer++;
+                }
+            }
+        }
+        return null;
+    }
+}
